d8d7db8f2d83814dc3fd040f3b9f273f9e5f6ef1
Huang-Jian-Rong <lvsonylg@gmail.com>
2023-02-03 14:39:33 +0800

    fix

--- pages/first-page.tsx
+++ pages/first-page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { Router, useRouter } from 'next/router';
 
 export default function FirstPost() {
@@ -6,8 +5,12 @@ export default function FirstPost() {
   const router = useRouter();
   return (
     <>
-      <h1>First Page</h1>
-      <button onClick={()=>router.push('./login')} className="justify-right text-xs border rounded-md bg-sky-600 text-white p-1 hover:bg-lime-600">Back to Login</button>
+      <h1 className='text-center text-sky-600 p-8'>First Page</h1>
+      <div className='flex justify-center'>
+        <button onClick={()=>router.push('./login')} className="text-xs border rounded-md bg-sky-600 text-white p-1 hover:bg-lime-600 flex ">Back to Login
+        </button>
+      </div>
+      
     </>
   );
 }
